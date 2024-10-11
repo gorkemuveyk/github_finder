68aca039042180a4a8eaa6de8176d8664466d0ec
Gorkem <uvyk@icloud.com>
2024-10-11 13:08:32 +0300

    styling 1 for user info

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const UserInfo = () => {
-  return <div className="bg-white w-full rounded-2xl p-6">UserInfo</div>;
+  return (
+    <div className="bg-white w-full rounded-2xl p-6">
+      <div className="flex justify-between">
+        <div className="w-48 h-48">
+          <img
+            src="https://placehold.co/200x200"
+            className="w-full h-full rounded-2xl"
+          />
+        </div>
+        <div className="w-1/2">
+          <h2 className="text-2xl font-bold">Adı Soyadı</h2>
+          <span className="text-xs italic font-semibold">@kullaniciadi</span>
+          <p className="text-sm mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quasi
+            nostrum maiores, magnam iusto praesentium laboriosam atque adipisci
+            cum? Vel recusandae
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserInfo;
