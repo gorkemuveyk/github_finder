9968534fb19414e184138dd44cfaa593dcb923fb
Gorkem <uvyk@icloud.com>
2024-10-11 11:44:48 +0300

    added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+    </>
+  );
 }
 
 export default App;
